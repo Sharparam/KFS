4dfd9aa34357d1a47bfc47f4572818b8fe6ea5f5
Adam Hellberg <sharparam@sharparam.com>
2017-04-13 17:51:29 +0200

    Add error codes to API

--- public_html/api.php
+++ public_html/api.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 require_once('../include/init.php');
 header('Content-Type: application/json');
 
@@ -7,8 +8,11 @@ try {
     $id = $_GET['id'];
     $movie = KFS\Movie::findById($id);
 
-    if ($movie === NULL)
-      throw new Exception("Unable to find movie with ID {$id}");
+    if ($movie === NULL) {
+      http_response_code(404)
+      echo json_encode(array('error' => "Unable to find movie with ID {$id}"));
+      return;
+    }
 
     echo json_encode($movie);
   } elseif (isset($_GET['mode']) && $_GET['mode'] == 'archive') {
@@ -28,6 +32,7 @@ try {
     if (KFS\Page::isPage($page)) {
       echo json_encode(KFS\Page::findByName($page));
     } else {
+      http_response_code(404);
       echo json_encode(array('error' => "{$page} is not a valid page"));
     }
   } else {
@@ -35,7 +40,9 @@ try {
     echo json_encode($movies);
   }
 } catch (PDOException $ex) {
+  http_response_code(500);
   echo json_encode(array('error' => $ex->getMessage(), 'code' => $ex->getCode()));
 } catch (Exception $ex) {
+  http_response_code(500);
   echo json_encode(array('error' => $ex->getMessage()));
 }
